dbc71321feecf65b7bd264c60524d2b8a9bdedfb
Andrew Stanton <up683693@myport.ac.uk>
2016-10-29 23:21:06 +0100

    Improved unit testing
    Content creation is now checked per propery. Not againts string.
    Expect used to ensure enought checks happen.
    Using modules to separate content creation from event handler checking.
    Callbacks use async.

--- t/firstTest.js
+++ t/firstTest.js
@@ -1,74 +1,83 @@
 /* globals QUnit, KeyboardEvent */
 let invokeKeypress = function(assert, event, element, message) {
+	let done = assert.async();
 	element.addEventListener('keypress', e => {
 		assert.deepEqual(e, event, message );
+		done();
 	});
 	element.dispatchEvent(event);
 };
 
-// This check that the system responds to <Mod?-Enter>
-QUnit.test('<Enter> in transclusionContainer', function( assert ) {
-	let element = document.querySelector('#main p');
-	invokeKeypress(assert,
-		new KeyboardEvent('keypress', {key: 'Enter'}),
-		element,
-		'<Enter> detected'
-	);
+QUnit.module('<*?-Enter> event detection', () =>{
+	QUnit.test('<Enter> in transclusionContainer', function( assert ) {
+		let element = document.querySelector('#main p');
+		invokeKeypress(assert,
+			new KeyboardEvent('keypress', {key: 'Enter'}),
+			element,
+			'<Enter> detected'
+		);
+	});
+
+	QUnit.test('<C-Enter> in transclusionContainer', function( assert ) {
+		let element = document.querySelector('#main p');
+		invokeKeypress(assert,
+			new KeyboardEvent('keypress', {key: 'Enter', ctrlKey: true}),
+			element,
+			'<C-Enter> detected'
+		);
+	});
+
+	QUnit.test('<S-Enter> in transclusionContainer', function( assert ) {
+		let element = document.querySelector('#main p');
+		invokeKeypress(assert,
+			new KeyboardEvent('keypress', {key: 'Enter', shiftKey: true}),
+			element,
+			'<S-Enter> detected'
+		);
+	});
+
+	QUnit.test('<A-Enter> in transclusionContainer', function( assert ) {
+		let element = document.querySelector('#main p');
+		invokeKeypress(assert,
+			new KeyboardEvent('keypress', {key: 'Enter', altKey: true}),
+			element,
+			'<A-Enter> detected'
+		);
+	});
 });
 
-QUnit.test('<C-Enter> in transclusionContainer', function( assert ) {
-	let element = document.querySelector('#main p');
-	invokeKeypress(assert,
-		new KeyboardEvent('keypress', {key: 'Enter', ctrlKey: true}),
-		element,
-		'<C-Enter> detected'
-	);
-});
-
-QUnit.test('<S-Enter> in transclusionContainer', function( assert ) {
-	let element = document.querySelector('#main p');
-	invokeKeypress(assert,
-		new KeyboardEvent('keypress', {key: 'Enter', shiftKey: true}),
-		element,
-		'<S-Enter> detected'
-	);
-});
-
-QUnit.test('<A-Enter> in transclusionContainer', function( assert ) {
-	let element = document.querySelector('#main p');
-	invokeKeypress(assert,
-		new KeyboardEvent('keypress', {key: 'Enter', altKey: true}),
-		element,
-		'<A-Enter> detected'
-	);
-});
-
-// Test content creation. Call and check result
-QUnit.test('createNewTransclusion', function( assert ) {
-	let expectedElem = '<blockquote contenteditable="true">Fetching A transclusion</blockquote>',
-		elem = document.enchiridion.transclusion.createNewTransclusion({
+QUnit.module('Content creation', () =>{
+	QUnit.test('createNewTransclusion', function( assert ) {
+		assert.expect(3);
+		let elem = document.enchiridion.transclusion.createNewTransclusion({
 			container: document.getElementById('qunit-fixture'),
 			innerText: 'A transclusion'
-		})
-	;
-	assert.deepEqual(elem.outerHTML, expectedElem, 'Create transclusion elelment');
-});
+		});
+		assert.equal(elem.tagName, 'BLOCKQUOTE', 'Transclusion has correct tag');
+		assert.equal(elem.innerText, 'Fetching A transclusion', 'Transclusion has correct content');
+		assert.ok(elem.getAttribute('contentEditable'), 'Transclusion is editable');
+	});
 
-QUnit.test('createNewParallel', function( assert ) {
-	let expectedElem = '<section class="parallel" contenteditable="true"></section>',
-		elem = document.enchiridion.transclusion.createNewParallel({
-				container: document.getElementById('qunit-fixture')
-		})
-	;
-	assert.deepEqual(elem.outerHTML, expectedElem, 'Create transclusion elelment');
-});
+	QUnit.test('createNewParallel', function( assert ) {
+		assert.expect(4);
+		let elem = document.enchiridion.transclusion.createNewParallel({
+					container: document.getElementById('qunit-fixture')
+		});
 
-QUnit.test('createParagraph', function( assert ) {
-	let expectedElem = '<p contenteditable="true">A transclusion</p>',
-		elem = document.enchiridion.transclusion.createParagraph({
+		assert.equal(elem.tagName, 'SECTION', 'Parallel has correct tag');
+		assert.equal(elem.innerText, '', 'Parallel has correct content');
+		assert.ok(elem.getAttribute('contentEditable'), 'Parallel is editable');
+		assert.ok(elem.classList.contains('parallel'), 'Parallel has .parallel');
+	});
+
+	QUnit.test('createParagraph', function( assert ) {
+		assert.expect(3);
+		let elem = document.enchiridion.transclusion.createParagraph({
 			container: document.getElementById('qunit-fixture'),
 			innerText: 'A transclusion'
-		})
-	;
-	assert.deepEqual(elem.outerHTML, expectedElem, 'Create transclusion elelment');
+		});
+		assert.equal(elem.tagName, 'P', 'Paragraph has correct tag');
+		assert.equal(elem.innerText, 'A transclusion', 'Paragraph has correct content');
+		assert.ok(elem.getAttribute('contentEditable'), 'Paragraphs are editable');
+	});
 });
