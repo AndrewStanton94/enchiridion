40a10bd1b2f42bd93b612f8fbe1f3deb57a79c9d
Andrew Stanton <up683693@myport.ac.uk>
2016-11-17 00:02:47 +0000

    QUnit now works
    Have local copies of QUnit src
    Copied a QUnit shim into the require config
    QUnit code moved out of index.ejs to a component
        Code called on window load.

--- src/plugins/config.js
+++ src/plugins/config.js
@@ -2,7 +2,18 @@
 requirejs.config({
     baseUrl: 'plugins',
     paths: {
+		qunit: '../dist/qunit/qunit'
+    },
+	shim: {
+       'qunit': {
+           exports: 'qunit',
+           init: function() {
+               QUnit.config.autoload = false;
+               QUnit.config.autostart = false;
+           }
+       }
     }
 });
+console.log(requirejs);
 
 requirejs(['testPlugin']);
