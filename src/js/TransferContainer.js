c9995ac0271493c2a8c8fb5a47694ae5000ec45e
Andrew Stanton <up683693@myport.ac.uk>
2017-02-05 19:54:26 +0000

    TransferContainer and fragmentLoader in project
    Copied from lab

---
+++ src/js/TransferContainer.js
@@ -0,0 +1,38 @@
+document.enchiridion = document.enchiridion || {};
+document.enchiridion.dataStructures = document.enchiridion.dataStructures || {};
+document.enchiridion.dataStructures.TransferContainer = class{
+	constructor(fragment, plugins, formatsToRender){
+		this._fragment = fragment;
+		this._plugins = plugins;
+		this._formatsToRender = formatsToRender;
+		this._element = null;
+	}
+
+	set data(recievedData){
+		this._data = recievedData;
+	}
+
+	get data(){
+		return this._data;
+	}
+
+	get plugins(){
+		return this._plugins;
+	}
+
+	get formatToRender(){
+		return this._formatsToRender[0];
+	}
+
+	get fragment(){
+		return this._fragment;
+	}
+
+	get element(){
+		return this._element;
+	}
+
+	set element(elem){
+		this._element = elem;
+	}
+};
