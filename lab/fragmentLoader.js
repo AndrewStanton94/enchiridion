c4517c2c5e17ce6c41b09a5e5cc0b6db9fa2f7a6
Andrew Stanton <up683693@myport.ac.uk>
2017-02-05 17:38:20 +0000

    Broke fragment loader code up
    scratchpadFragmentTypeSelection contains set up and dummy data
    The TransferContainer is now saved into a dataStructures object
    fragmentLoader contains the fragment retrieval chain

---
+++ lab/fragmentLoader.js
@@ -0,0 +1,79 @@
+document.enchiridion = document.enchiridion || {};
+document.enchiridion.fragmentLoader = {
+	retrieveFragment: function(id, fragments){
+		let matchesId = f => f.getFragmentId() === id;
+		let fragment = fragments.find(matchesId);
+		return fragment;
+	},
+
+	// like python `in`
+	inCollection: function(item, collection){
+		return collection.indexOf(item) >= 0;
+	},
+
+	filterFormats: function(list, sliceIndex, validOptions){
+		return list.filter(function(key){
+			return document.enchiridion.fragmentLoader.inCollection(key.split('/')[sliceIndex], validOptions);
+		});
+	},
+
+	selectFormat: function(fragment){
+		let renderType = fragment.getFormats();
+		if(renderType.length > 1){
+			renderType = document.enchiridion.fragmentLoader.filterFormats(renderType, 1, config.preferredLanguages);
+			console.log('After language filter: ', renderType);
+		}
+		if(renderType.length > 1){
+			renderType = document.enchiridion.fragmentLoader.filterFormats(renderType, 0, config.preferredFormats);
+			console.log('After type filter: ', renderType);
+		}
+		return renderType;
+	},
+
+	// A promise that will do the plugin lookup
+	getPlugin: function(formatToRender, fragment){
+		return new Promise(function(resolve, reject){
+			document.enchiridion = document.enchiridion || {};
+			document.enchiridion.plugins = document.enchiridion.plugins || {};
+			let fileType = formatToRender[0].split('/')[0];
+
+			let plugin = document.enchiridion.plugins[fileType];
+			if(plugin){
+				resolve(new document.enchiridion.dataStructures.TransferContainer(fragment, plugin, formatToRender));
+			}
+			else{
+				require([fileType], plugin => {
+					document.enchiridion.plugins[fileType] = plugin;
+					resolve(new document.enchiridion.dataStructures.TransferContainer(fragment, plugin, formatToRender));
+				});
+			}
+		});
+	},
+
+	extractContent: function(transferContainer){
+		transferContainer.data = transferContainer.plugins.data(transferContainer);
+		return transferContainer;
+	},
+
+	generateElements: transferContainer => {
+		transferContainer.element = transferContainer.plugins.main(transferContainer);
+		return transferContainer;
+	},
+
+	draw: function(transferContainer){
+		let content = transferContainer.element;
+		content.draggable = true;
+		content.id = transferContainer.fragment.getFragmentId();
+		content.dataset.format = transferContainer.formatToRender;
+		document.getElementsByTagName('main')[0].insertBefore(content, null);
+	},
+
+	// Get plugin, then use the data one and pass it to a renderer
+	processFragment: function(fragment) {
+		let formatToRender = document.enchiridion.fragmentLoader.selectFormat(fragment);
+		document.enchiridion.fragmentLoader.getPlugin(formatToRender, fragment)
+		.then(document.enchiridion.fragmentLoader.extractContent)
+		.then(document.enchiridion.fragmentLoader.generateElements)
+		.then(document.enchiridion.fragmentLoader.draw);
+	}
+};
--- lab/scratchpadFragmentTypeSelection.js
+++ lab/scratchpadFragmentTypeSelection.js
@@ -1,120 +1,6 @@
-const TransferContainer = class{
-	constructor(fragment, plugins, formatsToRender){
-		this._fragment = fragment;
-		this._plugins = plugins;
-		this._formatsToRender = formatsToRender;
-		this._element;
-	}
-
-	set data(recievedData){
-		this._data = recievedData;
-	}
-
-	get data(){
-		return this._data;
-	}
-
-	get plugins(){
-		return this._plugins;
-	}
-
-	get formatToRender(){
-		return this._formatsToRender[0];
-	}
-
-	get fragment(){
-		return this._fragment;
-	}
-
-	get element(){
-		return this._element;
-	}
-
-	set element(elem){
-		this._element = elem;
-	}
-},
-
-config = {
+const config = {
 	preferredFormats: [ 'txt', 'gif' ],
 	preferredLanguages: [ 'en' ]
-},
-
-retrieveFragment = function(id, fragments){
-	let matchesId = f => f.getFragmentId() === id;
-	let fragment = fragments.find(matchesId);
-	return fragment;
-},
-
-// like python `in`
-inCollection = function(item, collection){
-	return collection.indexOf(item) >= 0;
-},
-
-filterFormats = function(list, sliceIndex, validOptions){
-	return list.filter(function(key){
-		return inCollection(key.split('/')[sliceIndex], validOptions);
-	});
-},
-
-selectFormat = function(fragment){
-	let renderType = fragment.getFormats();
-	if(renderType.length > 1){
-		renderType = filterFormats(renderType, 1, config.preferredLanguages);
-		console.log('After language filter: ', renderType);
-	}
-	if(renderType.length > 1){
-		renderType = filterFormats(renderType, 0, config.preferredFormats);
-		console.log('After type filter: ', renderType);
-	}
-	return renderType;
-},
-
-// A promise that will do the plugin lookup
-getPlugin = function(formatToRender, fragment){
-	return new Promise(function(resolve, reject){
-		document.enchiridion = document.enchiridion || {};
-		document.enchiridion.plugins = document.enchiridion.plugins || {};
-		let fileType = formatToRender[0].split('/')[0];
-
-		let plugin = document.enchiridion.plugins[fileType];
-		if(plugin){
-			resolve(new TransferContainer(fragment, plugin, formatToRender));
-		}
-		else{
-			require([fileType], plugin => {
-				document.enchiridion.plugins[fileType] = plugin;
-				resolve(new TransferContainer(fragment, plugin, formatToRender));
-			});
-		}
-	});
-},
-
-extractContent = function(transferContainer){
-	transferContainer.data = transferContainer.plugins.data(transferContainer);
-	return transferContainer;
-},
-
-generateElements = transferContainer => {
-	transferContainer.element = transferContainer.plugins.main(transferContainer);
-	return transferContainer;
-},
-
-draw = function(transferContainer){
-	let content = transferContainer.element;
-	content.draggable = true;
-	content.id = transferContainer.fragment.getFragmentId();
-	content.dataset.format = transferContainer.formatToRender;
-	document.getElementsByTagName('main')[0].insertBefore(content, null);
-},
-
-// Get plugin, then use the data one and pass it to a renderer
-processFragment = function(fragment) {
-	let formatToRender = selectFormat(fragment);
-	getPlugin(formatToRender, fragment)
-	.then(extractContent)
-	.then(generateElements)
-	.then(draw);
 },
 
 test = function(){
@@ -145,7 +31,7 @@ test = function(){
 	f3.setFragmentId('29bc4de4');
 	f3.setData('gif/en', 'http://media.tumblr.com/tumblr_mco8geSmzK1r3a4b6.gif');
 
-	topLevelFragments.map(processFragment);
+	topLevelFragments.map(document.enchiridion.fragmentLoader.processFragment);
 };
 
 window.addEventListener('load', test);
