c757a8adbab68f9b573a8eeab8677dd2d23587ae
Andrew Stanton <up683693@myport.ac.uk>
2017-02-27 02:34:27 +0000

    Copied code to image/gif
    It works but code is now duplicated.
    Will remove common code to a core plugin library later

--- src/plugins/image/gif.js
+++ src/plugins/image/gif.js
@@ -3,14 +3,40 @@ define({
 		return transferContainer.fragment.getData(transferContainer.dataTypeToRender);
 	},
 	'main': function(transferContainer){
-		let element = document.createElement('img');
-		element.src = transferContainer.data;
-		return element;
+		let tContainer = document.createElement('section'),
+			name = transferContainer.fragment.getFragmentName();
+		if (name) {
+			let fragmentName = document.createElement('h1');
+			fragmentName.innerText = name;
+			fragmentName.classList.add('fragmentTitle');
+			tContainer.appendChild(fragmentName);
+		}
+		transferContainer.data.forEach(entry => {
+			let element = document.createElement('img');
+			if (entry.type) {
+				document.enchiridion.transclusion.load(entry, element);
+			} else {
+				element.src = transferContainer.data;
+			}
+			tContainer.appendChild(element);
+		});
+		return tContainer;
 	},
-	'search': function(fragment){
-		console.log(`search ui plugin for ${fragment}`);
+	'search': function(transferContainer){
+		let elem = document.createElement('p');
+		let name = transferContainer.fragment.getFragmentName();
+		if (!name) {
+			name = 'Placeholder';
+		}
+		elem.innerText = name;
+		return elem;
 	},
 	'sidebar': function(fragment){
 		console.log(`sidebar ui plugin for ${fragment}`);
+	},
+
+	'create': function () {
+		let fragment = document.enchiridion.fragmentUtils.castToFragment({});
+		return fragment.constructor('me');
 	}
 });
